fa2ed6aeb6af8dc78563267e82c6bad9f3e53c12
Ernesta Ippolitva <ippolitova.ernesta@gmail.com>
2025-06-04 19:12:04 +0200

    login attempt 3

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,11 +4,12 @@ from tasks.views import GoogleLogin, GoogleLoginCallback, LoginPage
 from django.conf import settings
 from django.conf.urls.static import static
 from tasks.views import login_redirect_view
+from tasks.views import BoardViewSet, ListViewSet, CardViewSet, CommentViewSet
+# from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
       path("", LoginPage.as_view(), name="home"),
       path('admin/', admin.site.urls),
-      path('api/', include('tasks.urls')), 
       path("auth/login/redirect/", login_redirect_view, name="login_redirect"),
       path("login/", LoginPage.as_view(), name="login"),
       path("api/v1/auth/", include("dj_rest_auth.urls")),
@@ -27,3 +28,13 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# router = DefaultRouter()
+# router.register(r'boards', BoardViewSet, basename='board')
+# router.register(r'lists', ListViewSet)
+# router.register(r'cards', CardViewSet)
+# router.register(r'comments', CommentViewSet)
+
+# urlpatterns = [
+#      path('', include(router.urls)),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
