246c8e4a90c03865e2739d2c1dd14027f35931fd
Ernesta Ippolitva <ippolitova.ernesta@gmail.com>
2025-06-12 11:42:09 +0200

    Adapter

---
+++ task_manager/adapter.py
@@ -0,0 +1,9 @@
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+class MySocialAccountAdapter(DefaultSocialAccountAdapter):
+    def populate_user(self, request, sociallogin, data):
+        user = super().populate_user(request, sociallogin, data)
+        user.email = data.get('email', '')
+        user.first_name = data.get('first_name', '')
+        user.last_name = data.get('last_name', '')
+        return user
